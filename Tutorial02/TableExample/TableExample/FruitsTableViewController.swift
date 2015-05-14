583d65c46df2f2dff6acd7bdc57506a65c5d7651
dany1468 <dany1468@gmail.com>
2015-05-14 22:48:36 +0900

    Add fruit data and edit button

---
+++ Tutorial02/TableExample/TableExample/FruitsTableViewController.swift
@@ -0,0 +1,84 @@
+//
+//  FruitsTableViewController.swift
+//  TableExample
+//
+//  Created by 團洋一 on 2015/05/14.
+//  Copyright (c) 2015年 dany1468. All rights reserved.
+//
+
+import UIKit
+
+class FruitsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      self.navigationItem.rightBarButtonItem = self.editButtonItem()
+      // self.tableView.editing = true
+    }
+
+    var data = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+        "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
+        "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
+        "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
+        "Pear", "Pineapple", "Raspberry", "Strawberry"]
+
+    // MARK: - UITableViewDataSource
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return data.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FruitCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
